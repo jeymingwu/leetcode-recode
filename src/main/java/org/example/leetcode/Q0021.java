cc034442f8b50e8bbb681792a4fc4ea1571fcc45
jeymingwu <qqschool@126.com>
2021-07-17 02:04:04 +0800

    update 20210717 每日一题————21. 合并两个有序链表

--- src/main/java/org/example/leetcode/Q0021.java
+++ src/main/java/org/example/leetcode/Q0021.java
@@ -14,21 +14,36 @@ public class Q0021 {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-        ListNode p = new ListNode();
-        ListNode head = p;
+        // 方法一：迭代
+//        ListNode p = new ListNode();
+//        ListNode head = p;
+//
+//        while (l1 != null || l2 != null) {
+//
+//            if ((l1 != null && l2 != null && l1.val <= l2.val) || (l2 == null)) {
+//                p.next = l1;
+//                l1 = l1.next;
+//            } else {
+//                p.next = l2;
+//                l2 = l2.next;
+//            }
+//            p = p.next;
+//        }
+//
+//        return head.next;
 
-        while (l1 != null || l2 != null) {
+        // 方法二：递归
 
-            if ((l1 != null && l2 != null && l1.val <= l2.val) || (l2 == null)) {
-                p.next = l1;
-                l1 = l1.next;
-            } else {
-                p.next = l2;
-                l2 = l2.next;
-            }
-            p = p.next;
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
-
-        return head.next;
     }
 }
