f0273ac0064549fe40e9bacc48ee2b3c6fd0676d
jeymingwu <qqschool@126.com>
2021-07-23 15:52:48 +0800

    update 20210723 每日一题————112. 路径总和

---
+++ src/main/java/org/example/leetcode/Q0112.java
@@ -0,0 +1,55 @@
+package org.example.leetcode;
+
+import java.util.Stack;
+
+/**
+ * 112. 路径总和
+ * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+ *
+ * 叶子节点 是指没有子节点的节点。
+ *
+ * https://leetcode-cn.com/problems/path-sum/
+ *
+ * @author : jeymingwu
+ * @date : 2021-07-23
+ **/
+
+public class Q0112 {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if (root == null) {
+            return false;
+        }
+
+        int sum = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        TreeNode prep = null;
+
+        while (!stack.empty()) {
+
+            TreeNode pop = stack.pop();
+
+            if (prep != null && (pop.left == prep || pop.right == prep || pop == prep)) {
+                sum -= pop.val;
+            } else {
+                sum += pop.val;
+                if (pop.left == null && pop.right == null && sum == targetSum) {
+                    return true;
+                } else {
+                    stack.push(pop);
+                    if (pop.right != null) {
+                        stack.push(pop.right);
+                    }
+                    if (pop.left != null) {
+                        stack.push(pop.left);
+                    }
+                }
+            }
+            prep = pop;
+        }
+
+        return false;
+    }
+}
