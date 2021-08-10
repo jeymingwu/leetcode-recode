622aee020471dc11aac60beb2f095f47771b27d4
jeymingwu <qqschool@126.com>
2021-08-11 01:08:33 +0800

    update 20210811 ————sql - 182. 查找重复的电子邮箱

--- src/main/java/org/example/sql/Q0182.sql
+++ src/main/java/org/example/sql/Q0182.sql
@@ -21,7 +21,28 @@
 -- 说明：所有电子邮箱都是小写字母。
 -- https://leetcode-cn.com/problems/duplicate-emails/
 
+-- 方法一：先 GROUP BY 分组，再用聚合函数 COUNT() 统计列数，最后再判断
 SELECT Email
 FROM Person
 GROUP BY Email
 HAVING count(email) > 1
+
+-- 亦可创建临时辅助表
+SELECT Email
+FROM (
+     SELECT Email, count(1) nums
+    FROM Person
+    GROUP BY email
+         ) AS temp_person
+WHERE nums > 1
+
+-- 方法二：自联结
+SELECT DISTINCT a.Email
+FROM Person a, Person b
+WHERE a.id != b.id AND a.Email = b.Email
+
+-- 【拓展：统计出现 n 次的数据】
+SELECT 列名
+FROM 表名
+GROUP BY 列名
+HAVING count(列名) > n
